5f119cc12f1f98628ad595a4176de401c2d6a457
bernardrayoso <rayosobernard@gmail.com>
2023-10-03 21:00:25 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+export default function App() {
+  return (
+    <div className="App">
+      <Accordion faqs={faqs} />
+    </div>
+  );
+}
+
+const Accordion = ({ faqs }) => {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, index) => (
+        <AccordionItem
+          key={index}
+          num={index}
+          title={faq.title}
+          text={faq.text}
+        />
+      ))}
+    </div>
+  );
+};
+
+const AccordionItem = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      onClick={() => setIsOpen(!isOpen)}
+      className={`item ${isOpen ? "open" : ""}`}
+    >
+      <p className="number"> {num < 9 ? `0${num + 1}` : num + 1} </p>
+      <p className="title"> {title} </p>
+      <p className="icon"> {isOpen ? "+" : "-"} </p>
+      {isOpen && (
+        <div className="content-box">
+          <p className="text"> {text} </p>
+        </div>
+      )}
+    </div>
+  );
+};
